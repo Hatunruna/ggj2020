1307b312041e5d59655346a7bb94883242346275
Julien Bernard <julien.bernard@univ-fcomte.fr>
2020-01-31 23:50:03 +0100

    add start()

---
+++ code/bits/common/PemProtocol.h
@@ -0,0 +1,33 @@
+#ifndef TMPGAME_PEM_PROTOCOL_H
+#define TMPGAME_PEM_PROTOCOL_H
+
+#include <gf/Id.h>
+#include <gf/SerializationOps.h>
+
+#include "Constants.h"
+#include "GameSettings.h"
+#include "PemTypes.h"
+#include "ProtocolData.h"
+
+using namespace gf::literals;
+
+namespace ggj {
+
+  /*
+   * server -> client
+   */
+
+  struct PemServerInitRole {
+    static constexpr gf::Id type = "PemServerInitRole"_id;
+    CrewType role;
+    CardType cards[3];
+  };
+
+  template<typename Archive>
+  Archive operator|(Archive& ar, PemServerInitRole& data) {
+    return ar | data.role | data.cards;
+  }
+
+}
+
+#endif // TMPGAME_PEM_PROTOCOL_H
---
+++ code/bits/server/Crew.cc
@@ -0,0 +1,42 @@
+#include "Crew.h"
+
+#include <cassert>
+
+namespace ggj {
+
+  int Crew::computeRebels(int32_t players) {
+    assert(4 <= players && players <= 8);
+
+    if (players <= 6) {
+      return 1;
+    }
+
+    return 2;
+  }
+
+  int Crew::computeProtectors(int32_t players) {
+    return players - computeRebels(players);
+  }
+
+  std::vector<CrewType> Crew::createCrew(int32_t players) {
+    std::vector<CrewType> crew;
+
+    int32_t protectors = computeProtectors(players);
+
+    for (int32_t i = 0; i < protectors; ++i) {
+      crew.push_back(CrewType::Protector);
+    }
+
+    int32_t rebels = computeRebels(players);
+
+    for (int32_t i = 0; i < rebels; ++i) {
+      crew.push_back(CrewType::Rebel);
+    }
+
+    return crew;
+  }
+
+
+}
+
+
