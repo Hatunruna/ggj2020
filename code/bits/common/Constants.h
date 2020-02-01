d8826432520e434bb1f5d6f91c675130447d1888
Benjamin Goodwin <goodb25@gmail.com>
2020-02-01 19:07:31 +0100

    Center Map, no zoom for the moment

--- code/bits/common/Constants.h
+++ code/bits/common/Constants.h
@@ -5,22 +5,22 @@
 
 namespace ggj {
 
-  constexpr gf::Vector2i InitialSize = { 1024, 768 };
-  constexpr unsigned UICharacterSize = 14;
-  constexpr float UILineSize = UICharacterSize * 2.0f;
+    constexpr gf::Vector2i InitialSize = { 1024, 768 };
+    constexpr unsigned UICharacterSize = 14;
+    constexpr float UILineSize = UICharacterSize * 2.0f;
 
-  constexpr std::size_t PlayersCount = 2;
-  constexpr const char *Service = "2020";
+    constexpr std::size_t PlayersCount = 2;
+    constexpr const char *Service = "2020";
 
-  constexpr std::size_t MaxPlayerNameLength = 15;
-  constexpr std::size_t MaxRoomNameLength = 15;
+    constexpr std::size_t MaxPlayerNameLength = 15;
+    constexpr std::size_t MaxRoomNameLength = 15;
 
-  constexpr gf::Vector2f WorldSize = { 3960.0f, 1500.0f };
-  constexpr gf::Vector2f ViewSize = { 1500.0f, 1500.0f };
-  constexpr float MinimalHeight = 750.0f;
-  constexpr float MaximalHeight = 2000.0f;
+    constexpr gf::Vector2f WorldSize = { 3960.0f, 1500.0f };
+    constexpr gf::Vector2f ViewSize = { 2000.0f, 2000.0f };
+    constexpr float MinimalHeight = 1500.0f;
+    constexpr float MaximalHeight = 1500.0f;
 
-  constexpr int MaxCards = 3;
+    constexpr int MaxCards = 3;
 }
 
 #endif // TMPGAME_CONSTANTS_H
