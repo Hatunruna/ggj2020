b5fe6089fc582ae1530dcadfa393fd7f393b6997
Arthur HUGEAT <hugeat.arthur@gmail.com>
2020-02-01 12:19:33 +0100

    [client] Add title card.

--- code/bits/client/PlayerInfo.cc
+++ code/bits/client/PlayerInfo.cc
@@ -29,17 +29,28 @@ namespace ggj {
     target.draw(text, states);
 
     gf::Vector2f CardRelativeSize = { 0.1f, 0.20f };
+    gf::Vector2f CardSize = coordinates.getRelativeSize(CardRelativeSize);
     gf::RectangleShape rect;
     rect.setColor(gf::Color::Yellow);
-    rect.setSize(coordinates.getRelativeSize(CardRelativeSize));
+    rect.setSize(CardSize);
 
     for (unsigned i = 0; i < m_cards.size(); ++i) {
       float xPosition =  0.05f + i * (CardRelativeSize.width + 0.05f);
       float yPosition =  0.75f;
       auto position = coordinates.getRelativePoint({xPosition, yPosition});
-
       rect.setPosition(position);
       target.draw(rect, states);
+
+      // Display card name
+      gf::Text text(cardTypeString(m_cards[i]), m_font);
+      text.setColor(gf::Color::Red);
+      text.setCharacterSize(coordinates.getRelativeCharacterSize(0.015f));
+      text.setParagraphWidth(CardSize.width - 0.02f * CardSize.width);
+      text.setPosition(position + 0.01f * coordinates.getRelativeSize(CardRelativeSize));
+      text.setAlignment(gf::Alignment::Center);
+      text.setAnchor(gf::Anchor::TopLeft);
+
+      target.draw(text, states);
     }
   }
 
