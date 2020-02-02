477bfd2d113b73a861a38dcbc9004cb36c6db2a8
VitizLaPeste <lucas.grosjean07@orange.fr>
2020-02-02 12:14:06 +0100

    Added Release resolution

--- code/bits/common/PemTypes.h
+++ code/bits/common/PemTypes.h
@@ -149,7 +149,7 @@ namespace ggj {
     ResolutionType type;
 
     bool bomb; // Examine
-    std::vector<gf::Id> members; // Hide, Track
+    std::vector<gf::Id> members; // Hide, Track , Release (if there are more than 1 prisoneer)
   };
 
   template<typename Archive>
